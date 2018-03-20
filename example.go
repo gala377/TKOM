72559d7a8d726e4348c4dd155e4368fef4089073
rafalgal <rafal.galczak@nask.pl>
2018-03-20 16:46:59 +0100

    Complete example

--- example.go
+++ example.go
@@ -8,56 +8,105 @@ import "sync"
 // we add it if any print was present in code
 import "fmt"
 
-// Simple functions are parsed to golang functions
-func func_exmpl() {
-	// But we don't define them
-	// We return them !
+// For functions we make a function creator first
+func __CONSTRUCT__func_exmpl() {
+	// First is function prelude
+	// Then we return the function
 	return func() {
+		// print defaults to format
 		fmt.Printf("Just an example")
 	}
 }
+//Then we make a function under proper name
+var func_exmpl = __CONSTRUCT__func_exmpl()
 
-
-func test() {
-	//creates mutex for functions to
-	//lock on in critical sections
-	m := &sync.Mutex{}
-	//As we pass everything by ref
-	//We need to remember about it in Go
+func ___CONSTRUCT__test() {
+	// for functions with critical keyword
+	// prelude creates one mutex for each critical block
+	// for function to lock on in critical sections
+	m1 := &sync.Mutex{}
+	// As we pass everything by ref
+	// We need to remember about it in Go
 	return func(a *int64) {
 		// lock/Unlock make critical scope
-		m.Lock()
-
+		m1.Lock()
 		a = a + 1
-
-		m.Unlock()
+		m1.Unlock()
 	} 
 }
+var test = ___CONSTRUCT__test()
 
 // We need to track the returns 
 // If there is one it needs to be
 // added to the function definition 
-func return_in_critical() int64 {
-	m := &sync.Mutex{}
+func __CONSTRUCT__return_in_critical() int64 {
+	m1 := &sync.Mutex{}
 	return func(a *int64) {
-		m.Lock()
+		m1.Lock()
 		a = a + 1 
 		// here we cought return return a - 1
-		m.Unlock()
+		m1.Unlock()
 		// we put it after unlock
 		return a - 1
 	}
 }
+var return_in_critical = __CONSTRUCT__return_in_critical()
 
-func main() {
-	//Only type we use is int64
-	int64 a = 1
-	//For concurrent blocks we make a new scope
-	{
-
+func __CONSTRUCT__multiple_critical(a) {
+	// One mutex for each of critical blocks
+	m1 := &sync.Mutex{}
+	m2 := &sync.Mutex{}
+	return func(a *int64) {
+		// A first section
+		fmt.Printf("Starting...")
+		m1.Lock()
+		a = a + 1
+		m1.Unlock()
+		fmt.Printf("Added...")
+		// And a second one
+		m2.Lock()
+		a = a - 1
+		m2.Unlock()
 	}
-	//as concurrent is sequential 
-	// we want to wait for it's end
-	// wich ends when all gorutines ends
-	// we use groups to know that 
+}
+var multiple_critical = __CONSTRUCT__multiple_critical()
+
+
+// main is special, we parse it to normal golang main
+func main() {
+	// Function call
+	func_exmpl()
+	// Only type we use is int64
+	int64 a = 1
+	// For concurrent blocks we make a new scope
+	{
+		// with wait group at the beggining
+		var wg sync.WaitGroup
+		// For each expression in concurrent block
+		// we create a new gorutine
+		// and add 1 to a wait group
+		wg.Add(1) 
+		go func() {
+			// we need to say the wait group that we are done
+			defer wg.Done()
+			// remember to pass a reference not a copy
+			test(&a)
+		}()
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			test(&a)
+		}()
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			test(&a)
+		}()
+		// as concurrent is sequential 
+		// we want to wait for it's end
+		// wich ends when all gorutines ends
+		// we use groups to know that 
+		wg.Wait()
+	}
+	fmt.Printf("Expected 4 got %v", a)
 }
